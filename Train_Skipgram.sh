428f6cfa9a2b3d832a64cdf81c0cfad0cb1641c1
diegor <uczcdra@ucl.ac.uk>
2020-08-29 18:31:33 +0100

    Correct loading of CUDA compilers

--- Train_Skipgram.sh
+++ Train_Skipgram.sh
@@ -20,16 +20,18 @@
 # 7. Your work *must* be done in $TMPDIR 
 cd $TMPDIR
 # 8. load the cuda module (in case you are running a CUDA program
-# module unload compilers mpi
-# module load compilers/gnu/4.9.2
-# module load cuda/9.0.176-patch4/gnu-4.9.2
+module unload compilers mpi
+module load compilers/gnu/4.9.2
+module load compilers/intel/2020/release
+module load cuda/10.1.243/gnu-4.9.2
+module load cudnn/7.5.0.56/cuda-10.1
 # 9. Run the application - the line below is just a random example.
 #virtualenv envs/skipgram_syns_env
 echo 'JOB DETAILS:'
 echo '$PATH:'
 echo $PATH
 # echo 'Available compilers'
-# module avail compilers
+echo module avail compilers
 echo '$HOME'
 echo $HOME
 #export PYTHONPATH=/home/uczcdra/python_src/Python-3.7.4
